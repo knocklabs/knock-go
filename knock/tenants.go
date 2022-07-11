0875e76ca2e4939aae9077d86652c6a1f18bf4d1
Meryl Dakin <meryl.k.dakin@gmail.com>
2022-07-11 11:36:58 -0400

    chore(kno-1566): add tenant interface

---
+++ knock/tenants.go
@@ -0,0 +1,151 @@
+package knock
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/go-querystring/query"
+	"github.com/pkg/errors"
+)
+
+// TenantsService is an interface for communicating with the Knock
+// Tenants API endpoints.
+type TenantsService interface {
+	List(context.Context, *ListTenantsRequest) ([]*Tenant, *PageInfo, error)
+	Get(context.Context, *GetTenantRequest) (*Tenant, error)
+	Set(context.Context, *SetTenantRequest) (*Tenant, error)
+	Delete(context.Context, *DeleteTenantRequest) error
+}
+type tenantsService struct {
+	client *Client
+}
+
+var _ TenantsService = &tenantsService{}
+
+func NewTenantService(client *Client) *tenantsService {
+	return &tenantsService{
+		client: client,
+	}
+}
+
+// Context structs
+type Tenant struct {
+	ID         string                 `json:"id"`
+	Properties map[string]interface{} `json:"properties"`
+	UpdatedAt  time.Time              `json:"updated_at"`
+	CreatedAt  time.Time              `json:"created_at"`
+}
+
+// Client structs
+type ListTenantsRequest struct {
+	PageSize int    `url:"page_size,omitempty"`
+	Cursor   string `url:"page_size,omitempty"`
+	Before   string `url:"before,omitempty"`
+	After    string `url:"after,omitempty"`
+	TenantID string `url:"tenant_id,omitempty"`
+	Name     string `url:"name,omitempty"`
+}
+
+type ListTenantsResponse struct {
+	Items    []*Tenant `json:"items"`
+	PageInfo *PageInfo `json:"page_info"`
+}
+
+type GetTenantRequest struct {
+	ID string
+}
+type GetTenantResponse struct {
+	Tenant *Tenant
+}
+
+type SetTenantRequest struct {
+	ID         string                 `json:"-"`
+	Properties map[string]interface{} `json:""`
+}
+type SetTenantResponse = GetTenantResponse
+
+type DeleteTenantRequest = GetTenantRequest
+
+func tenantAPIPath(tenantID string) string {
+	return fmt.Sprintf("v1/tenants/%s", tenantID)
+}
+
+const tenantsAPIBasePath = "/v1/tenants"
+
+func (ms *tenantsService) List(ctx context.Context, listReq *ListTenantsRequest) ([]*Tenant, *PageInfo, error) {
+	queryString, err := query.Values(listReq)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "error parsing request to list tenants")
+	}
+	path := fmt.Sprintf("%s/%s", tenantsAPIBasePath, queryString.Encode())
+
+	req, err := ms.client.newRequest(http.MethodGet, path, listReq)
+
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "error creating request to list tenants")
+	}
+	listRes := &ListTenantsResponse{}
+
+	_, err = ms.client.do(ctx, req, listRes)
+
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "error making request to list tenants")
+	}
+
+	return listRes.Items, listRes.PageInfo, nil
+}
+func (os *tenantsService) Get(ctx context.Context, getTenantRequest *GetTenantRequest) (*Tenant, error) {
+	path := tenantAPIPath(getTenantRequest.ID)
+
+	req, err := os.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for get tenant")
+	}
+
+	getTenantResponse := &GetTenantResponse{Tenant: &Tenant{}}
+	_, err = os.client.do(ctx, req, getTenantResponse.Tenant)
+	if err != nil {
+		return nil, errors.Wrap(err, "error making request for get tenant")
+	}
+
+	return getTenantResponse.Tenant, nil
+}
+
+func (os *tenantsService) Set(ctx context.Context, setTenantRequest *SetTenantRequest) (*Tenant, error) {
+	path := tenantAPIPath(setTenantRequest.ID)
+
+	if len(setTenantRequest.Properties) == 0 {
+		return nil, &Error{msg: "Must set at least one property"}
+	}
+
+	req, err := os.client.newRequest(http.MethodPut, path, setTenantRequest.Properties)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for set tenant")
+	}
+
+	setTenantResponse := &SetTenantResponse{Tenant: &Tenant{}}
+	_, err = os.client.do(ctx, req, setTenantResponse.Tenant)
+	if err != nil {
+		return nil, errors.Wrap(err, "error making request for set tenant")
+	}
+
+	return setTenantResponse.Tenant, nil
+}
+
+func (os *tenantsService) Delete(ctx context.Context, deleteTenantRequest *DeleteTenantRequest) error {
+	path := tenantAPIPath(deleteTenantRequest.ID)
+
+	req, err := os.client.newRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return errors.Wrap(err, "error creating request for delete tenant")
+	}
+
+	_, err = os.client.do(ctx, req, nil)
+	if err != nil {
+		return errors.Wrap(err, "error making request for delete tenant")
+	}
+
+	return nil
+}
