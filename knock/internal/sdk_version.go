e85b9876944bc38f3cf5b81025e9b597fa5036ad
Victor Lymar <victor@knock.app>
2024-05-21 13:45:29 -0700

    chore: improve comment

--- knock/internal/sdk_version.go
+++ knock/internal/sdk_version.go
@@ -1,5 +1,5 @@
 package internal
 
-// This is used for formatting the SDK's user agent. It should be updated before
+// SDKVersion is used to populate the sdk's user agent. It should be updated before
 // releasing new versions of the SDK. Eventually we can automate this process.
 const SDKVersion = "0.1.16"
